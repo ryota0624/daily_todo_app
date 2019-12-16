6f46fd09adc059dfb945314671806d30f6f8f883
ryota0624 <ryota.suzuki@kyash.co>
2019-12-16 21:50:14 +0900

    Date, DailyTodoListの実装

--- lib/todo/daily_todo_list.dart
+++ lib/todo/daily_todo_list.dart
@@ -1,8 +1,34 @@
 import 'package:daily_todo_app/todo/todo.dart';
 
-class Date {}
+class Date {
+  final int year;
+  final int month;
+  final int day;
+
+  Date({this.year, this.month, this.day});
+
+  static fromDateTime(DateTime date) {
+    return Date(year: date.year, month: date.month, day: date.day);
+  }
+
+  DateTime asDateTime() => DateTime(year, month, day);
+}
+
 class DailyTodoList {
-  Date _date;
-  List<Todo> _list;
+  final Date _date;
+  final List<Todo> _list;
+
+  DailyTodoList(this._date, this._list);
+
   // 未達成を引きついで次の日のリストを作る
+  DailyTodoList createNextDayTodoList() {
+    var remainedTodos = _list.where((e) => !e.isFinished());
+    return DailyTodoList(_date, remainedTodos);
+  }
+
+  factory DailyTodoList.initialize(Date date) {
+    return DailyTodoList(date, []);
+  }
+
+  isAllTodoFinished() => _list.every((el) => el.isFinished());
 }
