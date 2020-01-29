972a1a330a8d03dc07454f1b0aeb0fa8886bc34c
ryota0624 <ryota.suzuki@kyash.co>
2020-01-29 10:09:36 +0900

    ChangeNotiferでModelを実装

--- README.md
+++ README.md
@@ -20,3 +20,4 @@ samples, guidance on mobile development, and a full API reference.
 Future待ってぐるぐるwidget
 Swipeするサムシング
 NavigationService
+converiant/coverian
--- lib/widget/todo_create_form.dart
+++ lib/widget/todo_create_form.dart
@@ -3,6 +3,51 @@ import 'package:daily_todo_app/todo.dart';
 import 'package:daily_todo_app/usecase/create_todo.dart';
 import 'package:daily_todo_app/usecase/usecase.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+mixin Notifier {
+  void notifyListeners();
+}
+
+class MockChangeNotifier implements ChangeNotifier {
+  @override
+  void addListener(listener) {
+    // TODO: implement addListener
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  // TODO: implement hasListeners
+  bool get hasListeners => null;
+
+  @override
+  void notifyListeners() {
+    // TODO: implement notifyListeners
+  }
+
+  @override
+  void removeListener(listener) {
+    // TODO: implement removeListener
+  }
+}
+
+abstract class TodoCreationModel implements ChangeNotifier {
+  String _text;
+
+  void inputText(String text) {
+    _text = text;
+    notifyListeners();
+  }
+
+  String getText() => _text;
+}
+
+class TodoCreationModelFWidget extends TodoCreationModel with ChangeNotifier {
+}
 
 class TodoCreationConfirmed extends UiEvent {
   TodoCreationConfirmed(this.subject);
@@ -25,7 +70,6 @@ class TodoCreateForm extends StatefulWidget {
 }
 
 class TodoCreateFormState extends State<TodoCreateForm> {
-  String _inputText = '';
 
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form.
@@ -36,37 +80,37 @@ class TodoCreateFormState extends State<TodoCreateForm> {
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Row(
-        children: <Widget>[
-          Flexible(child: TextFormField(
-            onChanged: (text) {
-              _inputText = text;
-            },
-          )),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            child: RaisedButton(
-              onPressed: () {
-                if (_inputText.isEmpty) {
-                  return;
-                }
-                TodoCreationConfirmed(_inputText);
-                widget.inputPort.put(
-                  CreateTodoParam(
-                    listID: widget.listID,
-                    subject: _inputText,
-                    labels: [],
+    return Consumer<TodoCreationModelFWidget>(
+        builder: (_, model, __) => Form(
+              key: _formKey,
+              child: Row(
+                children: <Widget>[
+                  Flexible(child: TextFormField(
+                    onChanged: (text) {
+                       model.inputText(text);
+                    },
+                  )),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (model.getText().isEmpty) {
+                          return;
+                        }
+                        TodoCreationConfirmed(model.getText());
+                        widget.inputPort.put(
+                          CreateTodoParam(
+                            listID: widget.listID,
+                            subject: model.getText(),
+                            labels: [],
+                          ),
+                        );
+                      },
+                      child: const Text('Submit'),
+                    ),
                   ),
-                );
-              },
-              child: const Text('Submit'),
-            ),
-          ),
-        ],
-      ),
-    );
+                ],
+              ),
+            )); // Build a Form widget using the _formKey created above.)
   }
 }
